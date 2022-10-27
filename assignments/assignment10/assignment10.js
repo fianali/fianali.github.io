697e477aa8f24f95667b8bd57d512c4a99c64f8d
fianali <fontasia10@gmail.com>
2022-10-27 15:26:04 -0600

    added assignment10 files

---
+++ assignments/assignment10/assignment10.js
@@ -0,0 +1,19 @@
+//q1 and q2
+fetch('assignment10.json')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+
+const json
+
+
+
+//q3
+function addEmployee(employeeString) {
+    const employees = JSON.parse(employeeString);
+
+    //go to the end of the employee array
+    for (let i = employees.length; i < 5; i++) {
+        var newEmployee;
+    }
+
+}
