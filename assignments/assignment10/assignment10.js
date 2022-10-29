20799117e773ea7cca6b06d9f2e6f6860de9f73b
fianali <fontasia10@gmail.com>
2022-10-29 15:25:20 -0600

    finished q4-q6

--- assignments/assignment10/assignment10.js
+++ assignments/assignment10/assignment10.js
@@ -8,21 +8,21 @@ var companyData =
             "first name": "Sam",
             "department": "Tech",
             "designation": "Manager",
-            "salary": "40000",
+            "salary": 40000,
             "raise eligible": "true"
         },
         {
             "first name": "Mary",
             "department": "Finance",
             "designation": "Trainee",
-            "salary": "18500",
+            "salary": 18500,
             "raise eligible": "true"
         },
         {
             "first name": "Bill",
             "department": "HR",
             "designation": "Executive",
-            "salary": "21200",
+            "salary": 21200,
             "raise eligible": "false"
         }
     ]
@@ -35,7 +35,7 @@ const newEmployee = {
     "first name": "Anna",
     "department": "Tech",
     "designation": "Executive",
-    "salary": "25600",
+    "salary": 25600,
     "raise eligible": "false"
 };
 
@@ -43,8 +43,38 @@ companyData['employees'].push(newEmployee);
 console.log(companyData);
 
 //q4
-let totalSalary = 'the total salary is $';
-for (let i = 0; i < companyData.length; i++) {
-    console.log("hello" + i);
+let totalSalary = 0;
+for (let i = 0; i < companyData.employees.length; i++) {
+
+    let salary = companyData.employees[i].salary;
+    totalSalary += salary;
+
 }
-// console.log(totalSalary);
+console.log('the total salary is $' + totalSalary);
+
+//q5
+for (let i = 0; i < companyData.employees.length; i++) {
+
+    let raiseEligible = companyData.employees[i]["raise eligible"];
+    if (raiseEligible === 'true') {
+        let salary = companyData.employees[i].salary;
+
+        companyData.employees[i].salary = salary;
+        raiseEligible = 'false';
+        console.log(companyData.employees[i]["first name"] + "'s salary was originally $" + salary + ' but is now $' + salary * 1.1 + ', and their raise eligibility is now ' + raiseEligible);
+
+    }
+}
+
+//q6
+let employee = companyData.employees;
+for (let i = 0; i < employee.length; i++) {
+
+    employee[i].wfh = 'false';
+
+    if (employee[i]["first name"] === 'Anna' || employee[i]["first name"] === 'Sam') {
+        employee[i].wfh = 'true';
+    }
+
+    console.log(employee[i]["first name"] + "'s work from home status is " + employee[i].wfh);
+}
