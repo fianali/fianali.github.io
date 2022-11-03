633d892708322f640216c2f296b7c06e8e91bbf1
fianali <fontasia10@gmail.com>
2022-11-03 16:16:35 -0600

    added assignment11, generate a new quote from an api and count the number of likes

---
+++ assignments/assignment11/assignment11.js
@@ -0,0 +1,35 @@
+const quoteButton = document.querySelector('.new-quote');
+quoteButton.addEventListener('click', getQuote);
+
+const likeButton = document.querySelector('.twitter');
+likeButton.addEventListener('click', changeNumLikes);
+let numLikes = 0;
+
+const endpoint = 'http://quotes.stormconsultancy.co.uk/random.json';
+
+async function getQuote() {
+    console.log("test button click");
+    let text = await fetch(endpoint);
+    let response = await text.text();
+
+    let json = JSON.parse(response);
+
+    displayQuote(json['quote']);
+    // displayQuote(json['author']);
+}
+
+function displayQuote(x) {
+    // const quoteBox = document.querySelector('#js-quote-text');
+    // const textMessage = document.createTextNode(x);
+    // quoteBox.appendChild(textMessage);
+
+    document.getElementById('js-quote-text').textContent = x;
+}
+
+function changeNumLikes() {
+    let numLikesSection = document.getElementById('numLikes');
+    numLikes++;
+    numLikesSection.textContent = numLikes;
+}
+
+getQuote();
